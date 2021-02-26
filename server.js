db4bc887697cd944a34bbce3fd7913fa2435bc66
DEVESHDS <dev1710.sharma@gmail.com>
2021-02-26 19:21:24 +0530

    dependencies added

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 
 const PORT = process.env.port || 3000;
 
+const connectDB = require("./config/db");
+connectDB();
+
+app.use("/api/upload", require("./routes/files"));
+
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
 });
