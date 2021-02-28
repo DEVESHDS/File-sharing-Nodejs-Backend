c7838e1406d93270d3154c30c3ebeea2254170e1
DEVESHDS <dev1710.sharma@gmail.com>
2021-02-28 17:49:11 +0530

    download endpoint added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require("path");
 
 const PORT = process.env.port || 3000;
 
+app.use(express.static("public"));
+
 const connectDB = require("./config/db");
 connectDB();
 
@@ -14,6 +16,7 @@ app.set("view engine", "ejs");
 //Routes
 app.use("/api/files", require("./routes/files"));
 app.use("/files", require("./routes/show"));
+app.use("/files/download", require("./routes/download"));
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
